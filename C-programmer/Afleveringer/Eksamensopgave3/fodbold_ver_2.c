a497003f55a24842bfe30460221b45244ec256d4
Lichiwena <sfrand19@student.aau.dk>
2019-12-04 23:02:08 +0100

    Version 2 og 3 done. Version 3 mangler print funktion og qsort.

--- C-programmer/Afleveringer/Eksamensopgave3/fodbold_ver_2.c
+++ C-programmer/Afleveringer/Eksamensopgave3/fodbold_ver_2.c
@@ -23,28 +23,32 @@ typedef struct
 
 typedef struct
 {
-    char *team_name[4]; /* Max length of team name is 3 + \0 */
+    char team_name[4]; /* Max length of team name is 3 + \0 */
     int  points;
     int  goals_scored_by_team;
     int  goals_scored_vs_team;
     int  goal_difference;
 } team_struct;
 
-enum team_name {AaB = 0, ACH, AGF, BIF, EFB, FCK, FCM, FCN, HOB, OB, RFC, SDR, VB, VEN};
+enum team_name {AaB_index = 0, ACH_index, AGF_index, BIF_index, EFB_index, FCK_index, FCM_index,
+                FCN_index, HOB_index, OB_index, RFC_index, SDR_index, VB_index, VEN_index};
 
 void fill_in_team_array(team_struct team_array[], match_struct current_match, int is_home, char *team_name);
 void update_team_score(team_struct team_array[], match_struct current_match, int is_home, int team_index);
+int match_result(int team_a_goals, int team_b_goals);
+void fill_in_team_name(team_struct team_array[], char *team_name, int team_index);
 
 int main(void)
 {   
-    FILE *filepointer = fopen("kampe2018-2019.txt", "r");
     int i = 0;
+    FILE *filepointer = fopen("kampe2018-2019.txt", "r");
     match_struct match_array[TOTAL_MATCHES];
     char current_string[MAX_CURRENT_STRING_LENGTH];
     team_struct team_array[20]; /* REE */
 
     if (filepointer != NULL)
     {      
+        /* f get s == file get string                                                            */
         /* fgets læser ind til MAX_CURRENT_STRING_LENGTH på 60 eller til den finder en new line. */
         /* While current_string != NULL står der basically                                       */
         while (fgets(current_string, MAX_CURRENT_STRING_LENGTH, filepointer) != NULL){
@@ -61,17 +65,6 @@ int main(void)
             /* [i] skal stå efter match_array, da man i match array'et kigger efter f.eks.  */
             /* day_of_week på den i'de plads. F.eks. ville match_array[20].day_of_week være */
             /* day_of_week på den 20'ende plads i arrayet (linje 20 i txt filen)            */
-
-            printf("%s %s %s %s - %s %d - %d %d\n",
-                   match_array[i].day_of_week,
-                   match_array[i].date,
-                   match_array[i].time_of_match,
-                   match_array[i].home_team,
-                   match_array[i].away_team,
-                   match_array[i].home_team_score,
-                   match_array[i].away_team_score,
-                   match_array[i].spectators );
-
             ++i;
         }
     } else {
@@ -80,7 +73,6 @@ int main(void)
 
     for (i = 0; i < TOTAL_MATCHES; ++i)
     {
-        printf("i is: %d\n", i);
         /* Håndterer home team */
         fill_in_team_array(team_array, match_array[i], HOME, match_array[i].home_team);
 
@@ -90,58 +82,76 @@ int main(void)
 
     for (i = 0; i < TOTAL_TEAMS; ++i)
     {
-        printf("| %d %d |\n",
+        printf("| Name: %4s | Points %4d | Scored by team: %4d | Scored vs team: %4d | Goal difference: %4d |\n",
+                team_array[i].team_name,
+                team_array[i].points,
                 team_array[i].goals_scored_by_team,
-                team_array[i].goals_scored_vs_team
+                team_array[i].goals_scored_vs_team,
+                team_array[i].goal_difference
                 );
     }
+    fclose(filepointer);
+
     return (0);
 }
-
 
 void fill_in_team_array(team_struct team_array[], match_struct current_match, int is_home, char *team_name)
 {
    if (strcmp("AaB", team_name) == 0) {
-        update_team_score(team_array, current_match, is_home, AaB);
+        update_team_score(team_array, current_match, is_home, AaB_index);
+        fill_in_team_name(team_array, team_name, AaB_index);
 
    } else if (strcmp("ACH", team_name) == 0) {
-        update_team_score(team_array, current_match, is_home, ACH);
-   
+        update_team_score(team_array, current_match, is_home, ACH_index);
+        fill_in_team_name(team_array, team_name, ACH_index);
+
    } else if (strcmp("AGF", team_name) == 0) {
-        update_team_score(team_array, current_match, is_home, AGF);
+        update_team_score(team_array, current_match, is_home, AGF_index);
+        fill_in_team_name(team_array, team_name, AGF_index);
 
    } else if (strcmp("BIF", team_name) == 0) {
-        update_team_score(team_array, current_match, is_home, BIF);
+        update_team_score(team_array, current_match, is_home, BIF_index);
+        fill_in_team_name(team_array, team_name, BIF_index);
 
    } else if (strcmp("EFB", team_name) == 0) {
-        update_team_score(team_array, current_match, is_home, EFB);
+        update_team_score(team_array, current_match, is_home, EFB_index);
+        fill_in_team_name(team_array, team_name, EFB_index);
 
    } else if (strcmp("FCK", team_name) == 0) {
-        update_team_score(team_array, current_match, is_home, FCK);
+        update_team_score(team_array, current_match, is_home, FCK_index);
+        fill_in_team_name(team_array, team_name, FCK_index);
 
    } else if (strcmp("FCM", team_name) == 0) {
-        update_team_score(team_array, current_match, is_home, FCM);
+        update_team_score(team_array, current_match, is_home, FCM_index);
+        fill_in_team_name(team_array, team_name, FCM_index);
 
    } else if (strcmp("FCN", team_name) == 0) {
-        update_team_score(team_array, current_match, is_home, FCN);
+        update_team_score(team_array, current_match, is_home, FCN_index);
+        fill_in_team_name(team_array, team_name, FCN_index);
 
    } else if (strcmp("HOB", team_name) == 0) {
-        update_team_score(team_array, current_match, is_home, HOB);
+        update_team_score(team_array, current_match, is_home, HOB_index);
+        fill_in_team_name(team_array, team_name, HOB_index);
 
    } else if (strcmp("OB", team_name) == 0)  {
-        update_team_score(team_array, current_match, is_home, OB);
+        update_team_score(team_array, current_match, is_home, OB_index);
+        fill_in_team_name(team_array, team_name, OB_index);
 
    } else if (strcmp("RFC", team_name) == 0) {
-        update_team_score(team_array, current_match, is_home, RFC);
+        update_team_score(team_array, current_match, is_home, RFC_index);
+        fill_in_team_name(team_array, team_name, RFC_index);
 
    } else if (strcmp("SDR", team_name) == 0) {
-         update_team_score(team_array, current_match, is_home, SDR);
+        update_team_score(team_array, current_match, is_home, SDR_index);
+        fill_in_team_name(team_array, team_name, SDR_index);
 
    } else if (strcmp("VB", team_name) == 0)  {
-        update_team_score(team_array, current_match, is_home, VB);
+        update_team_score(team_array, current_match, is_home, VB_index);
+        fill_in_team_name(team_array, team_name, VB_index);
 
    } else if (strcmp("VEN", team_name) == 0) {
-        update_team_score(team_array, current_match, is_home, VEN);
+        update_team_score(team_array, current_match, is_home, VEN_index);
+        fill_in_team_name(team_array, team_name, VEN_index);
     }
 }
 
@@ -152,14 +162,41 @@ void update_team_score(team_struct team_array[], match_struct current_match, int
 {
     if (is_home == 1)
     {
+        team_array[team_index].points               += match_result(current_match.home_team_score, current_match.away_team_score);
         team_array[team_index].goals_scored_by_team += current_match.home_team_score;
         team_array[team_index].goals_scored_vs_team += current_match.away_team_score;
         team_array[team_index].goal_difference      += (current_match.home_team_score - current_match.away_team_score);
 
     } else if (is_home == 0)
     {
+        team_array[team_index].points               += match_result(current_match.away_team_score, current_match.home_team_score);
         team_array[team_index].goals_scored_by_team += current_match.away_team_score;
         team_array[team_index].goals_scored_vs_team += current_match.home_team_score;
         team_array[team_index].goal_difference      += (current_match.away_team_score - current_match.home_team_score);   
+    }
+}
+
+int match_result(int team_a_goals, int team_b_goals)
+{
+    int result = 0;
+
+    if (team_a_goals > team_b_goals)
+    {
+        result = 3;
+    } else if (team_a_goals == team_b_goals)
+    {
+        result = 1;
+    } else {
+        result = 0;
+    }
+
+    return result;
+}
+
+void fill_in_team_name(team_struct team_array[], char *team_name, int team_index)
+{
+    if (strcmp(team_array[team_index].team_name, team_name) != 0)
+    {
+        strcpy(team_array[team_index].team_name, team_name);
     }
 }
